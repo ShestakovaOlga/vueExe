07cc1367b982119bd30a71c50396e89658a68ea5
Olga Shestakova <olgashestakova@Air-de-Olga.home>
2020-02-14 18:00:44 +0100

    add

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template >
+  <div>
+    <p>Hello {{name}}</p>
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoList from "./TodoList.vue";
+
+export default {
+  components: {
+    TodoList
+  },
+  data() {
+    return {
+      name: "Olga"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+div{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+p {
+  color: green;
+}
+</style>
---
+++ TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">Hello {{name}}
+    <button v-on:click="counter += 1">Add 1</button>
+  <p>Se ha hecho clic en el botón de arriba {{ counter }} veces.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+
+export default Vue.extend({
+  data() {
+    return {
+      name: "Jairo",
+      counter: 0
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.container {
+  color:blue;
+  border:1px solid gray;
+  width: 500px;
+  padding: 10px;
+}
+</style>
